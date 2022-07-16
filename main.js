f9fa74d1cd301dc337ae1e35f2646cc456ca6455
Jesus <jdgonzalez815@misena.edu.co>
2022-07-16 13:28:29 -0500

    Primer commit - reloj en HTML, CSS y js

---
+++ main.js
@@ -0,0 +1,33 @@
+const displayTime=() => {
+  let date = new Date;
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second =date.getSeconds();
+  let amPM= ''
+
+
+  // am pm
+  hour < 12 ? amPm = 'AM' : amPm = 'PM'
+
+
+  // 12hr clock
+  if(hour >12){
+    hour -= 12
+  }
+
+  //padding
+  let padHour= String(hour).padStart(2, '0');
+  let padMin= String(minute).padStart(2, '0')
+  //  let padMin= String(second).padStart(2, '0')
+
+
+  //display
+  time.textContent = `${padHour}:${padMin} ${amPm}`;
+
+
+  // update in near real-time
+  requestAnimationFrame(displayTime);
+
+}
+
+displayTime();
